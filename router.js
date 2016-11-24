7dd855b6a7a7f20a0375806aea4057a3cc302b74
brouchey <benrouch@me.com>
2016-11-24 16:48:42 +0100

    fix plain text to html rendering

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ var renderer = require("./renderer.js");
 
 function home(request, response) {
     if(request.url == "/") {
-        response.writeHead(200, {'Content-type': "text/plain"});
+        response.writeHead(200, {'Content-type': "text/html"});
 		renderer.view("header", { }, response);     // { } because no values need to be passed
 		renderer.view("home", { }, response);
 		renderer.view("footer", { }, response);
@@ -12,7 +12,7 @@ function home(request, response) {
 
 function contact(request, response) {
     if(request.url == "/contact") {
-        response.writeHead(200, {'Content-type': "text/plain"});
+        response.writeHead(200, {'Content-type': "text/html"});
         renderer.view("header", { }, response);
 		renderer.view("contact", { }, response);
 		renderer.view("footer", { }, response);
@@ -22,7 +22,7 @@ function contact(request, response) {
 
 function about(request, response) {
     if(request.url == "/about") {
-        response.writeHead(200, {'Content-type': "text/plain"});
+        response.writeHead(200, {'Content-type': "text/html"});
         renderer.view("header", { }, response);
 		renderer.view("about", { }, response);
 		renderer.view("footer", { }, response);
