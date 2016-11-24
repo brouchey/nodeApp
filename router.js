c69e6c7c6a617144a9e9b70dffc824a676606cc6
brouchey <benrouch@me.com>
2016-11-24 15:59:05 +0100

    create views folder and some basic html files

--- router.js
+++ router.js
@@ -4,18 +4,21 @@ function home(request, response) {
         response.end("Home\n");
     }
 }
+
 function contact(request, response) {
     if(request.url == "/contact") {
         response.writeHead(200, {'Content-type': "text/plain"});
         response.end("Contact\n");
     }
 }
+
 function about(request, response) {
     if(request.url == "/about") {
         response.writeHead(200, {'Content-type': "text/plain"});
         response.end("About\n");
     }
 }
+
 module.exports.home = home;
 module.exports.contact = contact;
 module.exports.about = about;
